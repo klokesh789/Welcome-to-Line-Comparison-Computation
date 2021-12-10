2415c6cec8c22919d6c332de38106a29001ec3bd
lokesh <kottelokesh789@gmail.com>
2021-12-11 04:30:36 +0530

    ADDED: comparelength

--- linecomputation.java
+++ linecomputation.java
@@ -1,15 +1,31 @@
-public class linecomputation
+class LineComparisonComputation
 {
-    // Function to calculate distance
-static double distance(int x1, int y1, int x2, int y2)
-{
-    // Calculating distance
-    return Math.sqrt(Math.pow(x2 - x1, 2) +
-                Math.pow(y2 - y1, 2) * 1.0);
-}
-    //Driver code
-    public static void main (String[] args)
-    {
+
+	static double d,d1;
+	static double distance(int x1, int y1, int x2, int y2)
+	{
+
+	    d = Math.sqrt(Math.pow(x2 - x1, 2) +
+        	        Math.pow(y2 - y1, 2) * 1.0);
+	    return d;
+	}
+	static double distance1(int a1, int b1, int a2, int b2)
+	{
+
+    		d1 = Math.sqrt(Math.pow(a2 - a1, 2) +
+                Math.pow(b2 - b1, 2) * 1.0);
+   		 return d1;
+	}
+    	public static void main (String[] args)
+    	{
         System.out.println(Math.round(distance(3, 4, 4, 3)*100000.0)/100000.0);
+	System.out.println(Math.round(distance1(2, 5, 5, 2)*100000.0)/100000.0);
+        System.out.println(d == d1);
+	if (d>d1)
+	System.out.println("First line is greater");
+        else if (d<d1)
+        System.out.println("second line is greater");
+	else
+	System.out.println("Both are equals");
     }
 }
